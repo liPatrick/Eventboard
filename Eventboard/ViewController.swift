64cbe10baeada7a77d4e9b91add5ad4d0e946151
Patrick Li <patrick8289@gmail.com>
2019-02-09 15:07:38 -0800

    Initial Commit

---
+++ Eventboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eventboard
+//
+//  Created by Patrick Li on 2/9/19.
+//  Copyright © 2019 Patrick Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
