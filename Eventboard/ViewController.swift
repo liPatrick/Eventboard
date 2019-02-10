f536dd5a1d87cf8a4538faf99a418d6e7052ccc6
Patrick Li <patrickli@Patricks-MacBook-Pro.local>
2019-02-09 16:34:42 -0800

    first commit

--- Eventboard/ViewController.swift
+++ Eventboard/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import Foundation
+
+
+class Colors {
+    var gl:CAGradientLayer!
+    
+    init() {
+        let colorTop = UIColor(red: 247/255, green: 191/255, blue: 108/255, alpha: 1).cgColor
+        let colorBottom = UIColor(red: 255/255, green: 169/255, blue: 58/255, alpha: 1).cgColor
+        
+        self.gl = CAGradientLayer()
+        self.gl.colors = [colorTop, colorBottom]
+        self.gl.locations = [0.0, 1.0]
+    }
+}
 
 class ViewController: UIViewController {
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let colors = Colors()
+
+        view.backgroundColor = UIColor.clear
+        var backgroundLayer = colors.gl
+        backgroundLayer?.frame = view.frame
+        view.layer.insertSublayer(backgroundLayer!, at: 0)
+
+        
+        //background color
+       
+        /*var gl: CAGradientLayer
+
+        let colorTop = UIColor(red: 247/255, green: 191/255, blue: 108/255, alpha: 0.85)
+        let colorBottom = UIColor(red: 245, green: 181, blue: 89, alpha: 1)
+        
+        gl = CAGradientLayer()
+        gl.colors = [colorTop, colorBottom]
+        gl.locations = [0.0, 1.0]
+        
+        view.backgroundColor = UIColor.clear
+        let backgroundLayer = gl
+        backgroundLayer.frame = view.frame
+        view.layer.insertSublayer(backgroundLayer, at: 0)
+*/
     }
-
-
 }
 
