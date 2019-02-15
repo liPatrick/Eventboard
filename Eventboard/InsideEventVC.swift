4ae295ce90633175b462409f1f98d2980324d84e
Patrick Li <patrickli@Patricks-MacBook-Pro.local>
2019-02-14 22:25:10 -0800

    eventpage ui

--- Eventboard/InsideEventVC.swift
+++ Eventboard/InsideEventVC.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class EventTableViewCell: UITableViewCell {
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var bodytext: UILabel!
     
 }
 
@@ -25,7 +27,19 @@ class InsideEventVC: UIViewController, UITableViewDelegate, UITableViewDataSourc
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "EventTableViewCell", for: indexPath)
             as! EventTableViewCell
-        cell.label.text = "here"
+        cell.title.font = UIFont(name: "Quicksand-Bold", size: 15)
+        cell.bodytext.font = UIFont(name: "Quicksand-Light", size: 12)
+        cell.bodytext.text = "Email: danielbess16@gmail.com\nPhone: 4087969033\nWebsite: www.google.com"
+        cell.bodytext.textColor = UIColor(red: 0.14, green: 0.14, blue: 0.14, alpha: 1)
+        
+        
+        cell.cardView.layer.shadowColor = UIColor(red: 0.12, green: 0.13, blue: 0.25, alpha: 0.12).cgColor
+        cell.cardView.layer.shadowOpacity = 1
+        cell.cardView.layer.shadowOffset = CGSize.zero
+        cell.cardView.layer.shadowRadius = 5
+        
+        
+        
         return cell
     }
     
@@ -34,9 +48,10 @@ class InsideEventVC: UIViewController, UITableViewDelegate, UITableViewDataSourc
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 140
+        return 150
+        
     }
-
+    
     @IBOutlet weak var eventImage: UIImageView!
     @IBOutlet weak var eventTitle: UILabel!
     @IBOutlet weak var eventDescription: UILabel!
@@ -47,7 +62,7 @@ class InsideEventVC: UIViewController, UITableViewDelegate, UITableViewDataSourc
         
         //self.tableView.delegate = self
         //self.tableView.dataSource = self
-
+        
         //image gradient
         let colorLeft = UIColor(red: 0.12, green: 0.13, blue: 0.25, alpha: 1).cgColor
         let colorRight = UIColor(red: 0.12, green: 0.13, blue: 0.25, alpha: 0.05).cgColor
